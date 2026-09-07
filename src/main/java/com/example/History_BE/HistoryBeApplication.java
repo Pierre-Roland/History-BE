4937f5c18bb72c5e6a29289cb7ba0fcf73fd9df8
Pierre Roland <127956402+pierreroland04@users.noreply.github.com>
2026-09-07 15:27:15 +0200

    feat: first commit

---
+++ src/main/java/com/example/History_BE/HistoryBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.History_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/History_BE/HistoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.History_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
